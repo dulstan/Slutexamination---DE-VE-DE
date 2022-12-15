51087054a069f5d1131225ddad4705580e917352
dulstan <114564962+dulstan@users.noreply.github.com>
2022-12-15 21:06:26 +0100

    done!

--- DE-VE-DE/js/index.js
+++ DE-VE-DE/js/index.js
@@ -1,6 +1,13 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  addDoc,
+  deleteDoc,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +20,100 @@ const firebaseConfig = {
   storageBucket: "de-ve-de-9f88e.appspot.com",
   messagingSenderId: "169892086536",
   appId: "1:169892086536:web:baaf18eb103115eaa462bd",
-  measurementId: "G-31219G2HKD"
+  measurementId: "G-31219G2HKD",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+const inputTitleElem = document.querySelector("#title");
+const inputGenreElem = document.querySelector("#genre");
+const inputDateElem = document.querySelector("#date");
+const watchListBtn = document.querySelector("#watchList");
+
+const movieSection = document.querySelector("#movieSection");
+
+
+async function saveToDataBase(titleName, genreItem, dateItem) {
+  try {
+    await addDoc(collection(db, "toWatch"), {
+      title: titleName,
+      genre: genreItem,
+      date: dateItem,
+    });
+  } catch (error) {
+    console.log("ERROR", error);
+  }
+}
+
+async function getMovies() {
+  const toWatchMovies = await getDocs(collection(db, "toWatch"));
+
+  movieSection.innerHTML = "";
+  //console.log('####', toWatchMovies);
+
+  toWatchMovies.forEach((movie) => {
+    //console.log(movie.id);
+    //console.log(movie.data());
+
+    const wrapper = ` 
+          <article  class="watchListElem" "${movie.data()}">
+              <h3 class="movieName">${movie.data().title}</h3>
+              <p>Genre: ${movie.data().genre}</p>
+              <p class="dateSection">Release Date: <br>${movie.data().date}</p>
+              <button type="button" data-movie-id=${
+                movie.id
+              } class="deleteBTN">Ta bort filmen</button>
+          </article>
+          `;
+    movieSection.insertAdjacentHTML("beforeend", wrapper);
+  });
+
+  deleteMovieClick();
+}
+
+watchListBtn.addEventListener("click", () => {
+  const titleName = inputTitleElem.value;
+  const genreItem = inputGenreElem.value;
+  const dateItem = inputDateElem.value;
+  saveToDataBase(titleName, genreItem, dateItem);
+
+  inputTitleElem.value = "";
+  inputGenreElem.value = "";
+  inputDateElem.value = "";
+});
+
+function getMyList() {
+  const showBtn = document.querySelector("#showMyList");
+  showBtn.addEventListener("click", async () => {
+    getMovies();
+    toggleList();
+  });
+}
+getMyList();
+
+function toggleList() {
+  movieSection.classList.toggle("hide");
+}
+
+async function removeFromDataBase(towatchId) {
+  try {
+    await deleteDoc(doc(db, "toWatch", towatchId));
+  } catch (error) {
+    console.log("ERROR", error);
+  }
+}
+function deleteMovieClick() {
+  const btnRemove = document.querySelectorAll(".deleteBTN");
+  //console.log(btnRemove)
+
+  btnRemove.forEach((remove) => {
+    remove.addEventListener("click", async (event) => {
+      const towatchId = event.target.getAttribute("data-movie-id");
+      await removeFromDataBase(towatchId);
+      getMovies();
+      //console.log(towatchId)
+    });
+  });
+}
